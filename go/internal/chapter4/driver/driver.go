dd4eb7c0fd58986bb3c0c1e3ec5f7b74e325665c
Petr Pechkurov <petr.pechkurov@gmail.com>
2026-04-25 23:42:13 +0300

    Add challenges for driver

--- go/internal/chapter4/driver/driver.go
+++ go/internal/chapter4/driver/driver.go
@@ -4,6 +4,7 @@ import (
 	"exercices/internal/input"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func Main() {
@@ -15,14 +16,27 @@ func Main() {
 	age := 0
 	_, err := fmt.Scanf("%d", &age)
 	if err != nil {
-		fmt.Printf("invalid age: %s", err)
+		fmt.Printf("invalid age: %s\n", err)
+		os.Exit(1)
+	}
+	if age < 0 {
+		fmt.Println("invalid age: should be more than 0")
 		os.Exit(1)
 	}
 
-	msg := "You are "
-	if age < 16 {
-		msg += "not "
+	lookup := map[int][]string{
+		16: {"US"},
+		18: {"JP", "DE"},
 	}
 
-	fmt.Println(msg + "old enough to legally drive")
+	msg := "You are "
+	allowed, ok := lookup[age]
+	if !ok {
+		fmt.Println(msg + "not old enough to legally drive")
+		os.Exit(2)
+	}
+
+	fmt.Printf(
+		"%sold enough to legally drive in the following countries: %s\n",
+		msg, strings.Join(allowed, ", "))
 }
